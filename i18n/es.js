515e3488f94a0eb8694100c5c7e88090dfcc7f04
Alejandro Celaya <alejandro@alejandrocelaya.com>
2018-06-10 19:32:06 +0200

    Added Vue and Vuei18n in order to be able to translate curriculum

---
+++ i18n/es.js
@@ -0,0 +1,6 @@
+const esMessages = {
+  'Zaragoza. July the 4th, 1988': 'Zaragoza. 4 de Julio de 1988',
+  'Professional experience': 'Experiencia profesional',
+  'September 2016 - April 2018': 'Septiembre 2016 - Abril 2018',
+  'Responsible of the company services design, at software architecture level': 'Responsable del diseño de los servicios de la empresa, a nivel de arquitectura del software'
+};
